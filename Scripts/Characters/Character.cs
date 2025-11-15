ea57d98087fa62e63773406145301f803c284c92
TheImagineer0 <43724601+TheImagineer0@users.noreply.github.com>
2025-11-15 00:06:13 -0800

    Essentials

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -4,53 +4,53 @@ namespace CrossDimensions.Characters;
 
 public partial class Character : CharacterBody2D
 {
-    [Export]
-    public Controllers.CharacterController Controller { get; set; }
+	[Export]
+	public Controllers.CharacterController Controller { get; set; }
 
-    [Export]
-    public States.StateMachine MovementStateMachine { get; set; }
+	[Export]
+	public States.StateMachine MovementStateMachine { get; set; }
 
-    [Export]
-    public float Speed { get; set; } = 192f;
+	[Export]
+	public float Speed { get; set; } = 192f;
 
-    [Export]
-    public float JumpForce { get; set; } = 384f;
+	[Export]
+	public float JumpForce { get; set; } = 384f;
 
-    /// <summary>
-    /// The cloneable component that allows this character to be cloned or
-    /// merged. If null, this character cannot be cloned.
-    /// </summary>
-    [Export]
-    public CloneableComponent Cloneable { get; set; } = null;
+	/// <summary>
+	/// The cloneable component that allows this character to be cloned or
+	/// merged. If null, this character cannot be cloned.
+	/// </summary>
+	[Export]
+	public CloneableComponent Cloneable { get; set; } = null;
 
-    public Vector2 VelocityFromInput { get; set; } = Vector2.Zero;
+	public Vector2 VelocityFromInput { get; set; } = Vector2.Zero;
 
-    public Vector2 VelocityFromExternalForces { get; set; } = Vector2.Zero;
+	public Vector2 VelocityFromExternalForces { get; set; } = Vector2.Zero;
 
-    public int AvailableJumps { get; set; } = 1;
+	public int AvailableJumps { get; set; } = 1;
 
-    /// <summary>
-    /// Indicates whether a jump input has been buffered. This is used to allow
-    /// the character to hold the jump button slightly before landing and jump
-    /// as soon as they touch the ground, only if the player has exhausted their
-    /// available jumps.
-    /// </summary>
-    public bool IsJumpBuffered { get; set; } = false;
+	/// <summary>
+	/// Indicates whether a jump input has been buffered. This is used to allow
+	/// the character to hold the jump button slightly before landing and jump
+	/// as soon as they touch the ground, only if the player has exhausted their
+	/// available jumps.
+	/// </summary>
+	public bool IsJumpBuffered { get; set; } = false;
 
-    public float AirAcceleration { get; set; } = 128f;
+	public float AirAcceleration { get; set; } = 128f;
 
-    public override void _Ready()
-    {
-        MovementStateMachine.Initialize(this);
-    }
+	public override void _Ready()
+	{
+		MovementStateMachine.Initialize(this);
+	}
 
-    public override void _Process(double delta)
-    {
-        MovementStateMachine.Process(delta);
-    }
+	public override void _Process(double delta)
+	{
+		MovementStateMachine.Process(delta);
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        MovementStateMachine.PhysicsProcess(delta);
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		MovementStateMachine.PhysicsProcess(delta);
+	}
 }
