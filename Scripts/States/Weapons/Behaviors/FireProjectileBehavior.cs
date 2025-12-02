acad927704d528677d36b9acfd2383da29ea19d6
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2025-12-02 15:14:10 -0800

    Make projectile spawn at offset

--- Scripts/States/Weapons/Behaviors/FireProjectileBehavior.cs
+++ Scripts/States/Weapons/Behaviors/FireProjectileBehavior.cs
@@ -7,11 +7,15 @@ public partial class FireProjectileBehavior : WeaponState
     [Export]
     public PackedScene ProjectileScene { get; set; }
 
+    [Export]
+    public Vector2 Offset { get; set; } = new(0, -4);
+
     public override State Enter(State previousState)
     {
         var projectile = ProjectileScene.Instantiate<Entities.Projectile>();
         projectile.OwnerCharacter = Weapon.OwnerCharacter;
-        projectile.GlobalPosition = projectile.OwnerCharacter.GlobalPosition;
+        projectile.GlobalPosition = projectile.OwnerCharacter.GlobalPosition
+            + Offset;
         projectile.Rotation = Weapon.Target.Angle();
 
         // TODO: use a World class to add the projectile to the scene
