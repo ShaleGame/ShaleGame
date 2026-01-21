37f02ebc0a284feaa92efa07b813199cb527987c
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2026-01-21 01:25:49 -0800

    fix: Add unit test nodes to scene tree

--- Tests/States/StateMachineTest.cs
+++ Tests/States/StateMachineTest.cs
@@ -3,7 +3,7 @@ using Godot;
 using GdUnit4;
 using static GdUnit4.Assertions;
 
-namespace CrossedDimensions.Tests;
+namespace CrossedDimensions.Tests.States;
 
 [TestSuite]
 public partial class StateMachineTest
@@ -24,11 +24,15 @@ public partial class StateMachineTest
     public void StateMachine_ShouldSetContextAndEnterInitialState()
     {
         var machine = new CrossedDimensions.States.StateMachine();
+
         var contextNode = new Node();
+        contextNode.AddChild(machine);
 
         var initial = new DummyState();
         machine.AddChild(initial);
         machine.InitialState = initial;
+
+        AddNode(contextNode);
 
         machine.Initialize(contextNode);
 
@@ -51,6 +55,8 @@ public partial class StateMachineTest
         machine.AddChild(childA);
         machine.AddChild(childB);
 
+        AddNode(machine);
+
         machine.ChangeState<DummyState>();
         // Should pick the first DummyState child (childA)
         AssertThat(machine.CurrentState).IsEqual(childA);
@@ -64,15 +70,20 @@ public partial class StateMachineTest
     public void StateMachine_ShouldProcessAndHandleTransition()
     {
         var machine = new CrossedDimensions.States.StateMachine();
+        var contextNode = new Node();
         var s1 = new TestStateThatTransitions();
         var s2 = new DummyState();
         s2.Name = "TestSibling";
 
+        contextNode.AddChild(machine);
+
         machine.AddChild(s1);
         machine.AddChild(s2);
 
+        AddNode(contextNode);
+
         machine.InitialState = s1;
-        machine.Initialize(new Node());
+        machine.Initialize(contextNode);
 
         // s1.Process should return s2 to cause a transition
         machine.Process(0.016);
