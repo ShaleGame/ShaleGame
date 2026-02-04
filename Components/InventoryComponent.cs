8e70567d57116076c9eeac226cc20b5b71a07ef2
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2026-02-04 02:03:56 -0800

    Change Inventory and Weapon logic

--- Components/InventoryComponent.cs
+++ Components/InventoryComponent.cs
@@ -2,29 +2,81 @@ using Godot;
 using Godot.Collections;
 using CrossedDimensions.Characters;
 using CrossedDimensions.Items;
+using CrossedDimensions.Extensions;
 
 namespace CrossedDimensions.Components;
 
+/// <summary>
+/// Component that provides a simple inventory for characters. It discovers
+/// contained <see cref="ItemInstance"/> children and manages weapon
+/// activation/selection for contained <see cref="Weapon"/> instances.
+/// </summary>
 public partial class InventoryComponent : Node
 {
+    /// <summary>
+    /// Emitted when the equipped weapon changes. Provides the previous and the
+    /// currently equipped weapon (either may be null).
+    /// </summary>
     [Signal]
     public delegate void EquippedWeaponChangedEventHandler(Weapon previousWeapon, Weapon currentWeapon);
 
+    /// <summary>
+    /// The list of items owned by this inventory. Items are discovered from the
+    /// node's children during <see cref="_Ready"/> and added to this list.
+    /// </summary>
     [Export]
     public Array<ItemInstance> Items { get; set; } = new();
 
+    /// <summary>
+    /// The character that owns this inventory. This will be assigned in the
+    /// editor or can be set programmatically. Discovered weapons will have
+    /// their <see cref="Items.ItemInstance.OwnerCharacter"/> set to this value.
+    /// </summary>
+    [Export]
     public Character OwnerCharacter { get; private set; }
 
+    /// <summary>
+    /// The currently equipped weapon, or null if no weapon is equipped.
+    /// </summary>
     public Weapon EquippedWeapon { get; private set; }
 
-    public bool IsWeaponEquipped(Weapon weapon)
-        => EquippedWeapon == weapon;
+    /// <summary>
+    /// Checks if <paramref name="weapon" /> is currently equipped.
+    /// </summary>
+    /// <returns>
+    /// <c>true</c> when the provided weapon is the one currently equipped by
+    /// this inventory.
+    /// </returns>
+    public bool IsWeaponEquipped(Weapon weapon) => EquippedWeapon == weapon;
 
-    public void Initialize(Character owner)
+    /// <summary>
+    /// Scans the node's children for <see cref="ItemInstance"/> objects, adds
+    /// them to <see cref="Items"/>, and initializes any <see cref="Weapon"/>
+    /// children by setting their owner and marking them inactive by default.
+    /// </summary>
+    public override void _Ready()
     {
-        OwnerCharacter = owner;
+        foreach (var child in GetChildren())
+        {
+            if (child is ItemInstance item)
+            {
+                Items.Add(item);
+
+                if (item is Weapon weapon)
+                {
+                    weapon.OwnerCharacter = OwnerCharacter;
+                    weapon.IsActive = false;
+                }
+            }
+        }
     }
 
+    /// <summary>
+    /// Equip the specified weapon. This will deactivate the previously equipped
+    /// weapon (if any), activate the new weapon, update the weapon's
+    /// <see cref="ItemInstance.OwnerCharacter"/>, and emit the
+    /// <see cref="EquippedWeaponChangedEventHandler"/> signal.
+    /// </summary>
     public void EquipWeapon(Weapon weapon)
     {
         if (EquippedWeapon == weapon)
@@ -34,6 +86,13 @@ public partial class InventoryComponent : Node
 
         var previousWeapon = EquippedWeapon;
         EquippedWeapon = weapon;
+
+        if (previousWeapon is not null)
+        {
+            previousWeapon.IsActive = false;
+        }
+
+        weapon.IsActive = true;
 
         if (OwnerCharacter is not null)
         {
