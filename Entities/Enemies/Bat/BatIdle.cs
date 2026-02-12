4ddf758b7431564327d07eceb094f7fb963a813d
TheImagineer0 <43724601+TheImagineer0@users.noreply.github.com>
2026-02-11 21:41:01 -0800

    State machine scripts

--- Entities/Enemies/Bat/BatIdle.cs
+++ Entities/Enemies/Bat/BatIdle.cs
@@ -1,3 +1,4 @@
+using CrossedDimensions.Characters;
 using Godot;
 using System;
 
@@ -6,4 +7,41 @@ namespace CrossedDimensions.States;
 [GlobalClass]
 public partial class BatIdle : State
 {
+    [Export] public float DetectionRadius = 200f;
+
+    private Character _bat;
+    private Node2D _player;
+    private State _attacking;
+
+    public override State Enter(State previousState)
+    {
+        _bat = Context as Character;
+
+        // Get reference to Attacking state
+        _attacking = GetParent().GetNode<State>("Attacking");
+
+        // Get player reference
+        _player = GetTree().GetFirstNodeInGroup("Player") as Node2D;
+
+        // Tell movement state machine to hang
+        var movementSh = _bat?.GetNode<StateMachine>("MovementStateMachine");
+        movementSh?.ChangeState("Hanging");
+
+        return base.Enter(previousState);
+    }
+
+    public override State Process(double delta)
+    {
+        // Check if player is within detection radius
+        if (_bat != null && _player != null)
+        {
+            float distanceToPlayer = _bat.GlobalPosition.DistanceTo(_player.GlobalPosition);
+            if (distanceToPlayer <= DetectionRadius)
+            {
+                return _attacking;
+            }
+        }
+
+        return base.Process(delta);
+    }
 }
