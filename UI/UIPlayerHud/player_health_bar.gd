8b210980f9611f2e71a0ec174a22ed72d5c93938
Christopher Maldonado <cmaldonadogomez@nevada.unr.edu>
2026-02-13 16:39:55 -0800

    fix health bar display value
    Fixed the health bars so that they now display value of health correctly matches the player and clone's actual health. Also fixed the signal for merging so that it is emited after the merge and after the health gets recalulated.

--- UI/UIPlayerHud/player_health_bar.gd
+++ UI/UIPlayerHud/player_health_bar.gd
@@ -9,11 +9,13 @@ var _full_length = 100
 func update_health(value: int) -> void:
 	progress_bar.value = value
 	
-func set_health_bar_half() -> void:
+func set_health_bar_half(max_health: int) -> void:
 	progress_bar.size.x = _half_length
+	progress_bar.max_value = max_health
 
-func set_health_bar_full() -> void:
+func set_health_bar_full(max_health: int) -> void:
 	progress_bar.size.x = _full_length
+	progress_bar.max_value = max_health
 	
 func hide_health_bar() -> void:
 	visible = false;
