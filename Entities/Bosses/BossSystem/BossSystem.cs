5f12cdb516f85eabf88dd75954bd42d88cae069b
ExoticCrystal <112528095+1102-Bickel-Hunter@users.noreply.github.com>
2026-02-27 00:36:18 -0800

    Boss system (#177)
    * Started Boss system implementation
    
    * Implemented boss spawning
    
    * Boss script 1.0 implementation
    
    * Added boss class and finished up BossSystem script
    
    * Added dummy boss
    
    * Added door for boss system and test boss area
    
    * Fixed up boss system and door scripts
    
    * Formatted
    
    * Update Environment/BossSystem/BossSystemDoor.cs
    
    Co-authored-by: HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
    
    * Added boss room separated from boss system
    
    * Formatting again
    
    * Update Environment/BossSystem/BossRoom.cs
    
    Co-authored-by: HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
    
    * Update Entities/Bosses/BossSystem/BossSystem.cs
    
    Co-authored-by: HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
    
    * Update Environment/BossSystem/BossSystemDoor.cs
    
    Co-authored-by: HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>

---
+++ Entities/Bosses/BossSystem/BossSystem.cs
@@ -0,0 +1,80 @@
+using CrossedDimensions.Characters;
+using CrossedDimensions.Characters.Controllers;
+using CrossedDimensions.Components;
+using CrossedDimensions.Entities.Enemies;
+using CrossedDimensions.Saves;
+using Godot;
+
+
+namespace CrossedDimensions.Environment.BossSystem;
+
+public partial class BossSystem : Node2D
+{
+
+    // Boss spawning
+    [Export] PackedScene BossScene;
+    // If boss does not position itself, it will spawn at the position given here
+    // If it does need a position to spawn at, make sure to attach the position node in the editor
+    [Export] Node2D spawnPosition;
+    Character bossInstance;
+
+    // Signals for boss defeat and spawn
+    [Signal] public delegate void BossSpawnedEventHandler();
+    [Signal] public delegate void BossDefeatedEventHandler();
+
+    public bool bossDefeated = false; // This should be set based on the save system
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+
+
+    }
+
+    public void SpawnBoss()
+    {
+        if (BossScene != null && !bossDefeated)
+        {
+            bossInstance = BossScene.Instantiate<Character>();
+            AddChild(bossInstance);
+
+            if (spawnPosition != null)
+            {
+                bossInstance.Position = spawnPosition.Position;
+            }
+
+            bossInstance.Health.HealthChanged += (oldHealth) =>
+            {
+                if (!bossInstance.Health.IsAlive)
+                {
+                    OnBossDefeated();
+                }
+            };
+
+            EmitSignal(SignalName.BossSpawned);
+        }
+    }
+
+    public void OnBossDefeated()
+    {
+        bossDefeated = true;
+        EmitSignal(SignalName.BossDefeated);
+
+        CharacterController controller = bossInstance.Controller;
+        string name = "null";
+        if (controller != null && controller is EnemyController enemyComponent)
+        {
+            name = enemyComponent.EnemyComponent.EnemyName;
+        }
+
+        // Saves boss defeat state
+        // Make sure boss node is named appropriately
+        SaveManager.Instance.GetKeyOrDefault("bosses/" + name, true);
+
+    }
+}
---
+++ Environment/BossSystem/BossSystemDoor.cs
@@ -0,0 +1,40 @@
+using Godot;
+using CrossedDimensions.Characters;
+using CrossedDimensions.Environment.BossSystem;
+
+namespace CrossedDimensions.Environment.BossSystem;
+
+public partial class BossSystemDoor : Node2D
+{
+    [Export] BossSystem bossSystem;
+
+    // Make sure door area is off by default
+    public CollisionShape2D doorArea;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        doorArea = GetNode<CollisionShape2D>("StaticBody2d/CollisionShape2D");
+
+        bossSystem.BossSpawned += OnBossSpawned;
+        bossSystem.BossDefeated += OnBossDefeated;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+    private void OnBossSpawned()
+    {
+        GD.Print("Boss spawned, enabling door");
+
+        doorArea.CallDeferred("set_disabled", false);
+    }
+
+    private void OnBossDefeated()
+    {
+
+        doorArea.CallDeferred("set_disabled", true);
+    }
+}
