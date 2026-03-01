613eb2bd0a96ba0c0da0f09d003e54e05d1158e5
ExoticCrystal <hunterbickel2003@gmail.com>
2026-02-28 21:05:30 -0800

    Set up scripts

---
+++ Entities/Bosses/Siracus/Attacking.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+using CrossedDimensions.States;
+
+// Picks an attack to perform
+
+public partial class Attacking : State
+{
+}
---
+++ Entities/Bosses/Siracus/Shotgun.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+using CrossedDimensions.States;
+
+// Spits out burst of projectiles in a cone in front of Siracus
+
+public partial class Shotgun : State
+{
+}
