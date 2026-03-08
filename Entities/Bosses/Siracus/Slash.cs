196f379ffe88c3bdafc827c4ec3251909926e24c
ExoticCrystal <hunterbickel2003@gmail.com>
2026-03-07 23:42:48 -0800

    DebugSprites

---
+++ Entities/Bosses/Siracus/Slash.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+using CrossedDimensions.States;
+
+// if player gets too close to boss, it does a slash attack with its claws
+
+public partial class Slash : State
+{
+}
