6f952321c237a3083bee1354f21462a5d8aea8d7
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2026-04-04 03:17:04 -0700

    feature: Add scene name trigger (#233)
    Adds area title cards to the bottom left of the screen.

---
+++ CrossedDimensions.Tests/Environment/AreaManagerTest.cs
@@ -0,0 +1,35 @@
+using CrossedDimensions.Environment;
+using Shouldly;
+using Xunit;
+
+namespace CrossedDimensions.Tests.Environment;
+
+ [Collection("GodotHeadless")]
+public class AreaManagerTest
+{
+    private readonly GodotHeadlessFixedFpsFixture _godot;
+
+    public AreaManagerTest(GodotHeadlessFixedFpsFixture godot)
+    {
+        _godot = godot;
+    }
+
+    [Fact]
+    public void NotifyAreaTitleTriggerEntered_WithAreaData_EmitsSignal()
+    {
+        var manager = new AreaManager();
+        var data = new AreaData
+        {
+            Title = "Crystal Peak",
+            Subtitle = "Whispering Ridges"
+        };
+
+        AreaData emitted = null;
+        manager.AreaTriggerEntered += areaData => emitted = areaData;
+
+        manager.NotifyAreaTitleTriggerEntered(data);
+
+        emitted.ShouldNotBeNull();
+        emitted.ShouldBeSameAs(data);
+    }
+}
---
+++ Environment/Triggers/AreaTitleTrigger.cs
@@ -0,0 +1,42 @@
+using CrossedDimensions.Environment;
+using Godot;
+
+namespace CrossedDimensions.Environment.Triggers;
+
+[GlobalClass]
+public partial class AreaTitleTrigger : Area2D
+{
+    [Export]
+    public AreaData AreaData { get; set; }
+
+    public override void _Ready()
+    {
+        BodyEntered += OnBodyEntered;
+    }
+
+    public override void _ExitTree()
+    {
+        BodyEntered -= OnBodyEntered;
+    }
+
+    private void OnBodyEntered(Node body)
+    {
+        if (body is not Characters.Character character)
+        {
+            return;
+        }
+
+        if (character.Cloneable?.IsClone ?? true)
+        {
+            return;
+        }
+
+        if (AreaData is null)
+        {
+            GD.PushWarning($"AreaTitleTrigger '{Name}' does not have AreaData assigned.");
+            return;
+        }
+
+        AreaManager.Instance?.NotifyAreaTitleTriggerEntered(AreaData);
+    }
+}
