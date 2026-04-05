7486e1a32819478c46624f2b66dab0d0414cbbae
ExoticCrystal <hunterbickel2003@gmail.com>
2026-04-05 02:53:12 -0700

    Path can move back and forth

--- Entities/Neutral/PathPlatform/PathTrace.cs
+++ Entities/Neutral/PathPlatform/PathTrace.cs
@@ -15,22 +15,53 @@ public partial class PathTrace : State
     [Export]
     Path2D path;
 
-    private Character platform;
+    private Character _platform;
+
+    private bool _curveClosed = true;
+
+    private int _direction = 1;
 
     public override State Enter(State previousState)
     {
-        platform = Context as Character;
+        _platform = Context as Character;
 
-        platform.GlobalPosition = path.Curve.GetPointPosition(0);
+        _platform.GlobalPosition = path.Curve.GetPointPosition(0);
+
+        if (path.Curve.PointCount >= 2) {
+            _curveClosed = false;
+        }
+        else
+        {
+            var firstPos = path.Curve.GetPointPosition(0);
+            var lastPos = path.Curve.GetPointPosition(path.Curve.PointCount - 1);
+            _curveClosed = firstPos.IsEqualApprox(lastPos);
+        }
+
+
 
         return base.Enter(previousState);
     }
 
     public override State PhysicsProcess(double delta)
     {
-        pathFollow.Progress += platform.Speed * (float)delta;
+        pathFollow.Progress += _platform.Speed * (float)delta * _direction;
 
-        platform.GlobalPosition = pathFollow.GlobalPosition;
+        _platform.GlobalPosition = pathFollow.GlobalPosition;
+
+        if (!_curveClosed)
+        {
+            if (pathFollow.ProgressRatio >= 1)
+            {
+                _direction = -1;
+            } else if (pathFollow.ProgressRatio <= 0)
+            {
+                _direction = 1;
+            }
+            else
+            {
+                
+            }
+        }
         
         GD.Print("Progress: ", pathFollow.Progress);
 
