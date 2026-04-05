b2a2c40a5a001b16859f0a5aa2e6390405f488ed
ExoticCrystal <hunterbickel2003@gmail.com>
2026-04-05 02:42:55 -0700

    Added path platform script

--- Entities/Neutral/PathPlatform/PathTrace.cs
+++ Entities/Neutral/PathPlatform/PathTrace.cs
@@ -1,6 +1,40 @@
 using Godot;
+using CrossedDimensions.States;
 using System;
+using System.ComponentModel;
+using CrossedDimensions.Characters;
+
+namespace CrossedDimensions.Entities.Neutral.PathPlatform;
 
 public partial class PathTrace : State
 {
+    
+    [Export]
+    PathFollow2D pathFollow;
+
+    [Export]
+    Path2D path;
+
+    private Character platform;
+
+    public override State Enter(State previousState)
+    {
+        platform = Context as Character;
+
+        platform.GlobalPosition = path.Curve.GetPointPosition(0);
+
+        return base.Enter(previousState);
+    }
+
+    public override State PhysicsProcess(double delta)
+    {
+        pathFollow.Progress += platform.Speed * (float)delta;
+
+        platform.GlobalPosition = pathFollow.GlobalPosition;
+        
+        GD.Print("Progress: ", pathFollow.Progress);
+
+        return base.PhysicsProcess(delta);
+    }
+
 }
