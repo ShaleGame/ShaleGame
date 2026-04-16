e7f238bbc69a4e51a948853c51010c49e849417f
ExoticCrystal <hunterbickel2003@gmail.com>
2026-04-16 01:13:38 -0700

    Script and scene nest setup

---
+++ Entities/Enemies/IcicleBees/Active.cs
@@ -0,0 +1,10 @@
+using CrossedDimensions.Characters;
+using Godot;
+
+namespace CrossedDimensions.States.Enemies.IceBees;
+
+// Searches for player and if it finds them and is close to the hive, spawns multiple bees and then goes to cooldown. Once cooldown is done, goes back to active
+
+public partial class Active : State
+{
+}
