543356b7ea8a65e15b87e6684e1935f3053d0784
ExoticCrystal <hunterbickel2003@gmail.com>
2026-04-18 13:30:47 -0700

    Initialize scripts

---
+++ Entities/Bosses/Drill/Cooldown.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+using CrossedDimensions.States;
+using CrossedDimensions.Characters;
+
+namespace CrossedDimensions.Entities.Bosses.Drill;
+
+public partial class Cooldown : State
+{
+}
