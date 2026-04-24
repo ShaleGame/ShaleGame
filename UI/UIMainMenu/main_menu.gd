b92e31f9b815c27d80b52c01f7636d2c2f07d800
MincedMeatTheFirst <112461281+1109-MaldonadoGomez-ChristopherAndre@users.noreply.github.com>
2026-04-23 23:29:16 -0700

    feature: Add video settings (#237)
    Adds functionality to the video settings menu.

--- UI/UIMainMenu/main_menu.gd
+++ UI/UIMainMenu/main_menu.gd
@@ -1,6 +1,15 @@
 extends Control
 @onready var settings_menu : SettingsSelector = %SettingSelectMenu
 @onready var save_loader : SaveLoader = %SaveLoader
+@onready var menu_particles: GPUParticles2D = $Node2D/GPUParticles2D
+
+func _ready() -> void:
+	var settings_manager: SettingsManager = $/root/SettingsManager
+	if settings_manager.Current != null:
+		menu_particles.emitting = settings_manager.Current.VisualEffectsEnabled
+	else:
+		menu_particles.emitting = true
+
 func _on_start_buitton_pressed() -> void:
 	save_loader.OpenSaveLoader()
 
--- UI/UISettings/settings_video.gd
+++ UI/UISettings/settings_video.gd
@@ -1,5 +1,56 @@
 class_name SettingsVideo
 extends Control
 
+@onready var display_mode_dropdown: OptionButton = %DisplayModeDropdown
+@onready var screen_shake_toggle: CheckBox = %ScreenShakeToggle
+@onready var visual_effects_toggle: CheckBox = %VisualEffectsToggle
+@onready var hdr_toggle: CheckBox = %HDRToggle
+var settings_manager = null
+
+func _ready() -> void:
+	settings_manager = get_node_or_null("/root/SettingsManager")
+
+	if not _has_current_settings():
+		return
+
+	var current_settings = settings_manager.Current
+	display_mode_dropdown.select(settings_manager.Current.WindowMode)
+	screen_shake_toggle.button_pressed = current_settings.ScreenShakeEnabled
+	visual_effects_toggle.button_pressed = current_settings.VisualEffectsEnabled
+	hdr_toggle.button_pressed = current_settings.HdrEnabled
+
+func _on_display_mode_dropdown_item_selected(index: int) -> void:
+	if not _has_current_settings():
+		return
+	settings_manager.ApplyWindowMode(index)
+	settings_manager.Current.WindowMode = index
+	settings_manager.Save()
+
+func _on_screen_shake_toggle_toggled(button_pressed: bool) -> void:
+	if not _has_current_settings():
+		return
+	settings_manager.Current.ScreenShakeEnabled = button_pressed
+	settings_manager.Save()
+
+func _on_visual_effects_toggle_toggled(button_pressed: bool) -> void:
+	if not _has_current_settings():
+		return
+	settings_manager.Current.VisualEffectsEnabled = button_pressed
+	settings_manager.Save()
+
+func _on_hdr_toggle_toggled(button_pressed: bool) -> void:
+	if not _has_current_settings():
+		return
+	settings_manager.ApplyHdr(button_pressed)
+	settings_manager.Current.HdrEnabled = button_pressed
+	settings_manager.Save()
+
 func _on_back_button_pressed() -> void:
 	hide()
+
+func _has_current_settings() -> bool:
+	if settings_manager == null:
+		settings_manager = get_node_or_null("/root/SettingsManager")
+	if settings_manager == null:
+		return false
+	return settings_manager.Current != null
