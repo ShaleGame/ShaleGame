54d3ef641472523b65323dc37ea42b025e5d5e2b
HumanoidSandvichDispenser <25856867+HumanoidSandvichDispenser@users.noreply.github.com>
2026-04-26 00:23:23 -0700

    feature: Add keybinds (#291)
    Adds input actions remappable to keys in the settings menu. Closes #106

--- UI/UISettings/SettingsManager.cs
+++ UI/UISettings/SettingsManager.cs
@@ -6,12 +6,14 @@ namespace CrossedDimensions.UI.UISettings;
 public partial class SettingsManager : Node
 {
     private const string SettingsPath = "user://settings.tres";
+    private const string InputBindingsPath = "user://input_bindings.json";
 
     public SettingsResource Current { get; private set; }
 
     public override void _Ready()
     {
         LoadOrCreateSettings();
+        LoadInputBindings();
         ApplyWindowMode(Current.WindowMode);
         ApplyHdr(Current.HdrEnabled);
     }
@@ -24,6 +26,38 @@ public partial class SettingsManager : Node
         }
 
         return ResourceSaver.Save(Current, SettingsPath);
+    }
+
+    public Error SaveInputBindings()
+    {
+        var serializedBindings = new Godot.Collections.Dictionary();
+
+        foreach (string actionName in InputMap.GetActions())
+        {
+            if (!ShouldPersistAction(actionName))
+                continue;
+
+            var serializedEvents = new Godot.Collections.Array();
+            foreach (InputEvent inputEvent in InputMap.ActionGetEvents(actionName))
+            {
+                var serializedEvent = SerializeInputEvent(inputEvent);
+                if (serializedEvent.Count > 0)
+                {
+                    serializedEvents.Add(serializedEvent);
+                }
+            }
+
+            serializedBindings[actionName] = serializedEvents;
+        }
+
+        using FileAccess file = FileAccess.Open(InputBindingsPath, FileAccess.ModeFlags.Write);
+        if (file is null)
+        {
+            return FileAccess.GetOpenError();
+        }
+
+        file.StoreString(Json.Stringify(serializedBindings));
+        return Error.Ok;
     }
 
     public void ApplyWindowMode(int modeIndex)
@@ -62,6 +96,132 @@ public partial class SettingsManager : Node
 
         Current = new SettingsResource();
         Save();
+    }
+
+    private void LoadInputBindings()
+    {
+        if (!FileAccess.FileExists(InputBindingsPath))
+        {
+            return;
+        }
+
+        using FileAccess file = FileAccess.Open(InputBindingsPath, FileAccess.ModeFlags.Read);
+        if (file is null)
+        {
+            return;
+        }
+
+        Variant parsed = Json.ParseString(file.GetAsText());
+        if (parsed.VariantType != Variant.Type.Dictionary)
+        {
+            return;
+        }
+
+        var bindings = parsed.AsGodotDictionary();
+        foreach (Variant actionVariant in bindings.Keys)
+        {
+            string actionName = actionVariant.AsString();
+            if (!InputMap.HasAction(actionName) || !ShouldPersistAction(actionName))
+            {
+                continue;
+            }
+
+            if (bindings[actionName].Obj is not Godot.Collections.Array serializedEvents)
+            {
+                continue;
+            }
+
+            InputMap.ActionEraseEvents(actionName);
+
+            foreach (Variant serializedEventVariant in serializedEvents)
+            {
+                if (serializedEventVariant.Obj is not Godot.Collections.Dictionary serializedEvent)
+                {
+                    continue;
+                }
+
+                InputEvent inputEvent = DeserializeInputEvent(serializedEvent);
+                if (inputEvent is not null)
+                {
+                    InputMap.ActionAddEvent(actionName, inputEvent);
+                }
+            }
+        }
+    }
+
+    private static bool ShouldPersistAction(string actionName)
+    {
+        return !actionName.StartsWith("debug_") && !actionName.StartsWith("ui_");
+    }
+
+    private static Godot.Collections.Dictionary SerializeInputEvent(InputEvent inputEvent)
+    {
+        var serialized = new Godot.Collections.Dictionary();
+
+        if (inputEvent is InputEventKey keyEvent)
+        {
+            serialized["type"] = "key";
+            serialized["keycode"] = (int)keyEvent.Keycode;
+            serialized["physical_keycode"] = (int)keyEvent.PhysicalKeycode;
+            serialized["unicode"] = keyEvent.Unicode;
+            serialized["location"] = (int)keyEvent.Location;
+            serialized["shift_pressed"] = keyEvent.ShiftPressed;
+            serialized["alt_pressed"] = keyEvent.AltPressed;
+            serialized["ctrl_pressed"] = keyEvent.CtrlPressed;
+            serialized["meta_pressed"] = keyEvent.MetaPressed;
+            return serialized;
+        }
+
+        if (inputEvent is InputEventMouseButton mouseButtonEvent)
+        {
+            serialized["type"] = "mouse_button";
+            serialized["button_index"] = (int)mouseButtonEvent.ButtonIndex;
+            serialized["shift_pressed"] = mouseButtonEvent.ShiftPressed;
+            serialized["alt_pressed"] = mouseButtonEvent.AltPressed;
+            serialized["ctrl_pressed"] = mouseButtonEvent.CtrlPressed;
+            serialized["meta_pressed"] = mouseButtonEvent.MetaPressed;
+            return serialized;
+        }
+
+        return serialized;
+    }
+
+    private static InputEvent DeserializeInputEvent(Godot.Collections.Dictionary serializedEvent)
+    {
+        if (!serializedEvent.ContainsKey("type"))
+        {
+            return null;
+        }
+
+        string eventType = serializedEvent["type"].AsString();
+        if (eventType == "key")
+        {
+            return new InputEventKey
+            {
+                Keycode = (Key)(int)serializedEvent["keycode"],
+                PhysicalKeycode = (Key)(int)serializedEvent["physical_keycode"],
+                Unicode = (long)serializedEvent["unicode"],
+                Location = (KeyLocation)(long)serializedEvent["location"],
+                ShiftPressed = (bool)serializedEvent["shift_pressed"],
+                AltPressed = (bool)serializedEvent["alt_pressed"],
+                CtrlPressed = (bool)serializedEvent["ctrl_pressed"],
+                MetaPressed = (bool)serializedEvent["meta_pressed"]
+            };
+        }
+
+        if (eventType == "mouse_button")
+        {
+            return new InputEventMouseButton
+            {
+                ButtonIndex = (MouseButton)(long)serializedEvent["button_index"],
+                ShiftPressed = (bool)serializedEvent["shift_pressed"],
+                AltPressed = (bool)serializedEvent["alt_pressed"],
+                CtrlPressed = (bool)serializedEvent["ctrl_pressed"],
+                MetaPressed = (bool)serializedEvent["meta_pressed"]
+            };
+        }
+
+        return null;
     }
 
     private static DisplayServer.WindowMode WindowModeToDisplayServerMode(int modeIndex)
