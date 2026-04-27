65dde2859ab47cb1ad4d75aa3db82941d1d70217
ExoticCrystal <112528095+1102-Bickel-Hunter@users.noreply.github.com>
2026-04-26 21:09:47 -0700

    tweak: Add vents to Drill Boss (#297)
    Drill boss can only be damaged by freezing and then attacking each vent.
    
    There is a small intermission between every 2 vents.
    
    ---------
    
    Co-authored-by: HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>

---
+++ Entities/Bosses/Drill/Dead.cs
@@ -0,0 +1,29 @@
+using Godot;
+using CrossedDimensions.States;
+
+namespace CrossedDimensions.Entities.Bosses.Drill;
+
+/// <summary>
+/// The vent is dead and cannot be interacted with.
+/// </summary>
+public partial class Dead : State
+{
+    [Export] public CollisionShape2D HurtboxCollision { get; set; }
+    [Export] public CollisionShape2D FrozenBoxCollision { get; set; }
+    [Export] AnimatedSprite2D Sprite { get; set; }
+
+    [Signal] public delegate void VentDiedEventHandler();
+
+    public override State Enter(State previousState)
+    {
+        HurtboxCollision.SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+        FrozenBoxCollision.SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+
+        Sprite.Play("Broken");
+
+        EmitSignal("VentDied");
+
+        return base.Enter(previousState);
+    }
+
+}
---
+++ Entities/Bosses/Drill/Unfrozen.cs
@@ -0,0 +1,48 @@
+using Godot;
+using CrossedDimensions.States;
+using CrossedDimensions.Components;
+
+namespace CrossedDimensions.Entities.Bosses.Drill;
+
+/// <summary>
+/// The vent is unfrozen and can't be hurt.
+/// </summary>
+
+public partial class Unfrozen : State
+{
+
+    [Export] public FreezableComponent Freezeable { get; set; }
+    [Export] public CollisionShape2D HurtboxCollision { get; set; }
+    [Export] public CollisionShape2D FrozenBoxCollision { get; set; }
+    [Export] public State FrozenState { get; set; }
+    [Export] public AnimatedSprite2D Sprite { get; set; }
+
+    public override State Enter(State previousState)
+    {
+
+        HurtboxCollision.SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+        FrozenBoxCollision.SetDeferred(CollisionShape2D.PropertyName.Disabled, false);
+
+        Freezeable.Frozen -= Frozen; // Avoid duplicate signals
+        Freezeable.Frozen += Frozen;
+
+        Sprite.Play("Default");
+
+        return base.Enter(previousState);
+    }
+
+    public override void Exit(State nextState)
+    {
+        Freezeable.Frozen -= Frozen;
+
+        base.Exit(nextState);
+    }
+
+    private void Frozen(float timeLeft)
+    {
+        StateMachine parent = GetParent<StateMachine>();
+
+        parent.ChangeState(FrozenState);
+    }
+
+}
