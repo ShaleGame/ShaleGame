1e55b9026b2976a749232a2765c40025db258f7d
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2026-04-30 23:35:11 -0700

    fix!: Move HUD elements to PlayerHud.tscn
    Moves the GiveItemArea HUD to PlayerHud because an instance of the
    dialogue player is instnaitated for each GiveItemArea, which causes
    problems whenever the player presses the interact key if there are
    multiple GiveItemAreas, or if the player is lower in the scene tree
    compared to the GiveItemArea.

--- BoundingBoxes/GiveItemArea.gd
+++ BoundingBoxes/GiveItemArea.gd
@@ -3,28 +3,31 @@ extends Area2D
 class_name GiveItemArea
 
 @export var item_scene: PackedScene
-@export var item_get: GetItem
 
 func _ready() -> void:
-	# connect here instead of in the editor because this is a hard dependency
-	connect("body_entered", Callable(self, "_on_body_entered"))
-	item_get.connect("CloseWeaponUI", _on_close_weapon_ui)
+    # connect here instead of in the editor because this is a hard dependency
+    connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("Player"):
-		print("Player collided with item area")
-		if item_scene:
-			var inventory: InventoryComponent = body.Inventory
-			if inventory.HasWeaponFromScene(item_scene):
-				queue_free()
-				return
-			print("Giving item to player")
-			var item_instance = item_scene.instantiate()
-			item_get.Item = item_instance.ItemData
-			item_get.StartItemGet()
-			inventory.add_child(item_instance)
-			inventory.EquipWeapon(item_instance, true)
+    if body.is_in_group("Player"):
+        if body.Cloneable.IsClone:
+            return
+        print("Player collided with item area")
+        if item_scene:
+            var inventory: InventoryComponent = body.Inventory
+            if inventory.HasWeaponFromScene(item_scene):
+                queue_free()
+                return
+            print("Giving item to player")
+            var item_instance = item_scene.instantiate()
+            var hud = body.get_node("%PlayerHud")
+            var item_get = hud.get_node("GetItem")
+            item_get.connect("CloseWeaponUI", _on_close_weapon_ui)
+            item_get.Item = item_instance.ItemData
+            item_get.StartItemGet()
+            inventory.add_child(item_instance)
+            inventory.EquipWeapon(item_instance, true)
 
 func _on_close_weapon_ui() -> void:
-	print("Close signal received")
-	queue_free()
+    print("Close signal received")
+    queue_free()
