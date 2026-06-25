77b1613dac4c649aa0c48700f28fa33d6f2dac76
chris <christopherm1204468@gmail.com>
2026-06-24 21:09:14 -0700

    added health bar textures

--- UI/UIPlayerHud/player_health_bar.gd
+++ UI/UIPlayerHud/player_health_bar.gd
@@ -1,16 +1,15 @@
 class_name PlayerHealthBar
 extends Control
 
-@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var progress_bar: TextureProgressBar = $TextureProgressBar
 @onready var icon: TextureRect = $CharacterIcon
-var _half_length = 50
 var _full_length = 100
 
 func update_health(value: int) -> void:
 	progress_bar.value = value
 	
 func set_health_bar_half(max_health: int) -> void:
-	progress_bar.size.x = _half_length
+	progress_bar.size.x = _full_length
 	progress_bar.max_value = max_health
 
 func set_health_bar_full(max_health: int) -> void:
