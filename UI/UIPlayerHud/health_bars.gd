91b4ca75238e1459caaa9d7b57bd23cb8b7f9b8d
chris <christopherm1204468@gmail.com>
2026-07-05 16:03:24 -0700

    fixed health bar visuals

--- UI/UIPlayerHud/health_bars.gd
+++ UI/UIPlayerHud/health_bars.gd
@@ -5,4 +5,15 @@ extends Control
 #pass them as variables
 @onready var main_health_bar: PlayerHealthBar = %PlayerHealthBar
 @onready var clone_health_bar: PlayerHealthBar = %CloneHealthBar
-@onready var heal_pool_bar: ProgressBar = %HealPoolBar
+@onready var heal_pool_bar: TextureProgressBar = %HealPoolBar
+@onready var character_icon: TextureRect = %CharacterIcon
+@onready var margin_container_2: MarginContainer = %MarginContainer2
+
+func set_icon_texture(texture: Texture2D) -> void:
+	character_icon.texture = texture
+
+func show_margin_container_2() -> void:
+	margin_container_2.visible = true
+
+func hide_margin_container_2() -> void:
+	margin_container_2.visible = false
